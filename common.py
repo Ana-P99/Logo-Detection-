74a771fe117c3ff550124dfdbec6f51b2258ae32
Ananya <66422530+Ana-P99@users.noreply.github.com>
2021-04-15 11:18:29 +0530

    Add files via upload

---
+++ common.py
@@ -0,0 +1,9 @@
+__all__ = ('CLASS_NAME')
+
+CLASS_NAME = [
+    'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
+    'Ford', 'Google', 'HP', 'Heineken', 'Intel', 'McDonalds', 'Mini', 'Nbc',
+    'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
+    'Texaco', 'Unicef', 'Vodafone', 'Yahoo'
+]
+
